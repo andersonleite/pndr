6ee948712b652325e37d0db1f19bd8165172b6c3
Anderson Leite <anderson_leite@apple.com>
2020-03-25 19:14:17 -0700

    interactions

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,16 @@
-console.log('setup ok');
+const poppa = document.querySelector('#poppa');
+
+const letters = document.querySelectorAll('.poppa-letter');
+
+poppa.addEventListener("mouseover", (event)=>{
+  letters.forEach((l)=>{
+    l.style.marginLeft = '200px'
+  })
+})
+
+poppa.addEventListener("mouseout", (event)=>{
+  letters.forEach((l)=>{
+    l.style.marginLeft = '0px'
+  })
+})
+
