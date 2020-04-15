2db42d801bf8c4595c3c703f66d74418277b0b81
Anderson Leite <anderson_leite@apple.com>
2020-04-15 12:46:25 -0700

    SBGs

--- src/js/main.js
+++ src/js/main.js
@@ -1,44 +1,57 @@
-// lOGO
-// ==========================================================================
-const poppa = document.querySelector('img.logo');
-const poppaContainer = document.querySelector('.poppa-container');
-poppa.addEventListener("mouseover", (event)=>{
+let x1 = x2 = 0; // left/right limit
+let y1 = y2 = 0; // top/bottom limit
 
-  // start logo anim
-  poppa.classList.add('animate')
+const poppa1 = document.querySelector('.poppa-1');
+const poppa2 = document.querySelector('.poppa-2');
+const poppa3 = document.querySelector('.poppa-3');
+const poppa4 = document.querySelector('.poppa-4');
+const poppa5 = document.querySelector('.poppa-5');
 
-  // start letters anim
-  // for( var i = 0; i <= 4; i++ ) {
-  //   var letter = document.querySelector( '.poppa-' + i );
-  //   letter.classList.add( 'animate' );
-  // }
+// define bounderies
+window.onload = function() {
 
-})
+  // horizontal bounderies
+  x1 = (window.innerWidth * .23);
+  x2 = window.innerWidth - x1;
 
-// poppaContainer.addEventListener("animationend", (event)=> {
-poppaContainer.addEventListener("mouseout", (event)=> {
+  // vertical boundaries
+  y1 = (window.innerHeight * .25);
+  y2 = 2 * y1;
 
-  // stop logo anim
-  poppa.classList.remove('animate')
-  // poppa.classList.add('poppa-init')
+};
 
-  // stop letters anim
-  // for( var i = 0; i <= 4; i++ ) {
-  //   var letter = document.querySelector( '.poppa-' + i );
-  //   letter.classList.remove( 'animate' );
-  // }
+let flicker = 0;
 
-})
+const poppa = new (function() {
+  this.anim = function(event){
+    event.preventDefault();
 
+    // const v = 0.5;
+    const v = Math.random(10);
 
-// LETTERS
-// ==========================================================================
-// for( var i = 0; i <= 4; i++ ) {
-//   var letter = document.querySelector( '.poppa-' + i );
-//   letter.addEventListener( 'mouseover', function() {
-//     this.classList.add( 'animate' );
-//   });
-//   letter.addEventListener( 'animationend', function() {
-//     this.classList.remove( 'animate' );
-//   });
-// }
+      if(Math.abs(flicker-event.pageX)>100) {
+
+        // LERP
+        if (event.pageX < x1 || event.pageX > x2) {
+          poppa1.style.transform = `perspective(500px) translate3d(0px, 0, 0px)`;
+          poppa2.style.transform = `perspective(500px) translate3d(0px, 0, 0px)`;
+          poppa3.style.transform = `perspective(500px) translate3d(0px, 0, 0px)`;
+          poppa4.style.transform = `perspective(500px) translate3d(0px, 0, 0px)`;
+          poppa5.style.transform = `perspective(500px) translate3d(0px, 0, 0px)`;
+
+        } else {
+          poppa1.style.transform = `translate3d(-${x1 * v}px, ${y1 * v}px, 0px) rotate(-45deg)`;
+          poppa2.style.transform = `translate3d(${x1 / 2 * v}px, -${y1 * v}px, 0px) rotate(45deg)`;
+          poppa3.style.transform = `translate3d(${x1 * v}px, ${y1 / 2 * v}px, 0px) rotate(180deg)`;
+          poppa4.style.transform = `translate3d(${x1 / 2 * v}px, ${y1 * v}px, 0px) rotate(-150deg)`;
+          poppa5.style.transform = `translate3d(-${x1 * v}px, -${y1 * v}px, 0px) rotate(-150deg)`;
+
+        }
+        flicker = event.pageX
+      }
+
+    }
+})();
+
+// will be letters.forEach...
+document.onmousemove = poppa.anim;
