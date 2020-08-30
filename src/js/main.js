030cd338ede60da44fcbfbd93e361a3c395450b1
Anderson Leite <anderson_leite@apple.com>
2020-08-30 10:40:17 -0700

    pndr

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,7 @@
 import fullpage from 'fullpage.js'
 
 var fullPageInstance = new fullpage('#fullpage', {
+  licenseKey: '4DE177FC-0AFC45BD-B70D5540-7C513D8C',
   navigation: true,
   sectionsColor:['#fff', '#fff', '#fff', '#fff']
 });
