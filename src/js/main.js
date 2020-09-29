170ed6260fa43d9b6a40191e35fce974ed021b66
Anderson Leite <anderson_leite@apple.com>
2020-09-29 08:49:32 -0700

    i18n

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,30 @@
 import fullpage from 'fullpage.js'
+import Translator from "./translator";
 
+// load fullPage.js
 var fullPageInstance = new fullpage('#fullpage', {
   licenseKey: '4DE177FC-0AFC45BD-B70D5540-7C513D8C',
   navigation: true,
   sectionsColor:['#fff', '#fff', '#fff', '#fff']
 });
 
+// load i18n/translations
+var translator = new Translator({
+  persist: false,
+  languages: ["en", "pt"],
+  defaultLanguage: "en",
+  detectLanguage: true,
+  filesLocation: "/i18n"
+});
 
-// //methods
-// fullpage_api.setAllowScrolling(false);
+translator.load();
+
+window.onload = function (){
+  translator.load(navigator.language);
+}
+
+// document.querySelector("form").addEventListener("click", function(evt) {
+//   if (evt.target.tagName === "INPUT") {
+//     translator.load(evt.target.value);
+//   }
+// });
